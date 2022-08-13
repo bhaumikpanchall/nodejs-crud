eb07cf3cd2843dd0214f0fe6a4498b8c61058e1a
BHAUMIK PANCHAL <84496973+bhaumikpanchall@users.noreply.github.com>
2022-08-13 13:29:57 +0530

    first commit

---
+++ config/db.js
@@ -0,0 +1,19 @@
+
+const mysql = require("mysql");
+
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connection = mysql.createConnection({
+    host : process.env.HOST,
+    user : process.env.USER,
+    password : process.env.PASSWORD,
+    database : process.env.DATABASE,
+});
+
+connection.connect((error) => {
+    if (error) throw error;
+    console.log("Connection Successfully");
+});
+
+module.exports = connection;
---
+++ server.js
@@ -0,0 +1,112 @@
+const express = require('express');
+const app = express();
+const dotenv = require("dotenv");
+var bodyParser = require("body-parser");
+
+dotenv.config();
+
+app.set("view engine","ejs");
+
+const connection = require("./config/db");
+
+app.use(express.static(__dirname + "/public"));
+app.use(express.static(__dirname + "/views"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.get("/", (req,res) => {
+    res.redirect("/create.html");
+});
+
+
+app.get("/delete-data", (req,res) => {
+
+    const deleteQuery = "delete from youtube_table where id=?";
+
+    connection.query(deleteQuery, [req.query.id], (err, rows) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.redirect("/data");
+        }
+    });    
+});
+
+app.get("/update-data", (req,res) => {
+
+    connection.query("Select * from youtube_table where id = ?", [req.query.id], (err, eachRow) => {
+        if(err) {
+            console.log(err);
+        }
+        else {
+            result= JSON.parse(JSON.stringify(eachRow[0]));
+
+            console.log(result);
+            res.render("edit.ejs", { result });
+        }
+    })
+})
+
+app.get("/data", (req,res) => {
+    connection.query(
+        "Select * from youtube_table", (err,rows) => {
+            if (err) {
+                console.log(err);
+            } else {
+                res.render("read.ejs", {rows});
+            }
+        }
+    )
+})
+
+//update
+app.post("/final-update", (req,res) => {
+
+    const id = req.body.hidden_id;
+    const name = req.body.name;
+    const email = req.body.email;
+
+    try{
+        connection.query(
+            "update youtube_table set name=?, email=? where id=?",
+            [name, email, id],
+            (err, rows) => {
+              if (err) {
+                console.log(err);
+              } else {
+                res.redirect("/data");
+              }
+             });
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+
+//create
+app.post("/create", (req,res) => {
+
+    const name = req.body.name;
+    const email = req.body.email;
+
+    try{
+        connection.query(
+            "INSERT into youtube_table (name,email) values(?,?)",
+            [name, email],
+            (err, rows) => {
+              if (err) {
+                console.log(err);
+              } else {
+                res.redirect("/data");
+              }
+             });
+    } catch (err) {
+        console.log(err);
+    }
+})
+
+app.listen(process.env.PORT || 4000, (error) => {
+    if (error) throw error;
+
+    console.log(`Server running on ${process.env.PORT}`);
+});
